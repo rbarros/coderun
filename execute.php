92c7d2480d957f23331c9e2c03c966693c018f6b
Ramon Barros <ramon@ezoom.com.br>
2013-08-08 15:03:23 -0300

    Coderun executando.

--- execute.php
+++ execute.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "This file executes the code";
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+if( isset($_REQUEST['index_php']) and strlen($_REQUEST['index_php'])>0 ){
+   $contents = $_POST['index_php'];
+   $contents=preg_replace("/<\?php?|\?>/", "", $contents); 
+   eval($contents); 
+}
